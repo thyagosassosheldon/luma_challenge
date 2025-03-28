4698f16c8cd8760169bf32957d0e6c3c03d206b7
Thyago Sasso Sheldon <54820143+thyagosassosheldon@users.noreply.github.com>
2025-03-28 15:19:44 -0300

    Add: HomePage scenarios

--- cypress/e2e/home_page/HomePage.ts
+++ cypress/e2e/home_page/HomePage.ts
@@ -10,10 +10,40 @@ Then("I should verify the Home Page loads sucessfully", () => {
 });
 
 Then("I should verify the Navigation Bar section", () => {
+	cy.get("nav#navbar").should("be.visible").within(() => {
+      cy.get("a.navbar-logo").should("have.attr", "title", "luma");
+
+      cy.get("ul.main-menu-v2").should("be.visible");
+
+      cy.get("div.actions").get("a#header_login").should("be.visible").and("have.text", "Log in");
+	  cy.get("div.actions").get("a.primary").should("be.visible").and("have.text", "Get a demo");
+    })
 });
 
 Then("I should verify Hero section", () => {
+	cy.get("div.hero-text-container").within(() => {
+		cy.get("h1.hero-section-title").should("be.visible").and("contain.text", "Manual Tasks’ Worst Nightmare");
+	
+		cy.get("p.hero-section-subtitle").should("be.visible");
+	
+		cy.get("div.hero-button-group").within(() => {
+		  cy.get("a[title='Build your demo']").should("be.visible").and("have.text", "Build your demo");
+	
+		  cy.get("a[title='See what it does']").should("be.visible").and("have.text", "See what it does");
+		});
+	  });
 });
 
 Then("I should verify Footer section", () => {
+	cy.get('.footer-copy-content').within(() => {
+
+		cy.get('div.left-content').within(() => {
+			cy.get("ul.f-sub-menu").should("be.visible");
+			cy.get("div.copy").should("be.visible");
+			});
+
+		cy.get('div.social-menu').within(() => {
+			cy.get("ul.f-social").should("be.visible");
+		});
+	})
 });
